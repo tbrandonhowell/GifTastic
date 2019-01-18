158d468621ca6bd609dd666b703477c4a5c6e782
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-01-17 21:28:19 -0500

    might be done, but probably not

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 
 // declaring variables variables:
 
-var pullLimit = 10; // # of gifs we're requesting
+var pullLimit = 50; // # of gifs we're requesting from API
+var scrubbedLimit = 10; // # of gifs we'll write that match our aspect ratio
+var scrubbedCount = 0; // # of matches we've had to our aspect ratio (used in the click function)
+var aspectHigh = 1.78; // upper limit of our aspect ratio matching
+var aspectLow = 1; // lower limit of our aspect ratio matching
 
 // create our array of topics
 
@@ -22,7 +26,7 @@ var writeButtons = function() {
     $("#buttons").empty(); // clear out the #buttons div
     for (i=0;i<topics.length;i++) { // loop through the array
         var newButton = $("<button>"); // create the new button
-        newButton.addClass("gifButton"); // give gifButton class
+        newButton.addClass("gifButton btn btn-danger m-1"); // give gifButton class
         newButton.attr("data-value",topics[i]); // assign the search term to the button
         newButton.text(topics[i]); // write the button name
         $("#buttons").append(newButton); // add the button to the buttons div
@@ -39,22 +43,37 @@ writeButtons();
 $(document).on("click", ".gifButton", function() { // watch for the click from one of the buttons
     console.log("word click captured"); // console log
     $("#images").empty(); // clear out the images div
+    scrubbedCount = 0; // reset scrubbed count
+    var offset = Math.floor(Math.random() * 20); // get our random offset so our images are a little different each time
     var searchTerm = $(this).attr("data-value"); // capture the search term
-    $.get("https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&limit=" + pullLimit + "&api_key=IZubHW9HEDubsdACK9Q1xxiHHbfKg99c").then(function(response) { // poll the API and put returning info into the response variable
+    $.get("https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&limit=" + pullLimit + "&offset=" + offset + "&api_key=IZubHW9HEDubsdACK9Q1xxiHHbfKg99c").then(function(response) { // poll the API and put returning info into the response variable
         console.log(response); // log the data returned
         for(i=0;i<response.data.length;i++) { // loop through the returned array
-            console.log("entry " + i);
-            var newDiv = $("<div>"); // make a wrapper for image + rating
-            newDiv.addClass("gifDiv"); // add this for styling
-            var newImg = $("<img>"); // make the img tag
-            newImg.attr("src",response.data[i].images.fixed_width_still.url); // img src
-            newImg.attr("id",response.data[i].id); // img id
-            newImg.attr("altImg", response.data[i].images.fixed_width.url); // alternate image URL for the swap
-            newImg.addClass("gifClick"); // add so we can watch for the click later
-            newDiv.append(newImg); // add the image to this image's wrapper div
-            var newRating = $("<p>").text(response.data[i].rating); // create the <p> with the rating info
-            newDiv.append(newRating); // add the rating into the newDiv
-            $("#images").append(newDiv); // add the newDiv to the images div on the screen
+            var width = response.data[i].images.original.width; // get width
+            var height = response.data[i].images.original.height; // get height
+            var ratio = width / height; // get ratio
+            console.log(width);
+            console.log(height);
+            console.log(ratio);
+            if (ratio < aspectHigh && ratio > aspectLow && scrubbedCount < scrubbedLimit) {
+                console.log("entry " + i);
+                var newDiv = $("<div>"); // make a wrapper for image + rating
+                newDiv.addClass("gifDiv"); // add this for styling
+                var newImg = $("<img>"); // make the img tag
+                newImg.attr("src",response.data[i].images.fixed_width_still.url); // img src
+                newImg.attr("id",response.data[i].id); // img id
+                newImg.attr("altImg", response.data[i].images.fixed_width.url); // alternate image URL for the swap
+                newImg.addClass("gifClick img-thumbnail m-3"); // add so we can watch for the click later
+                newDiv.append(newImg); // add the image to this image's wrapper div
+                var newRating = $("<p>").text("Rating: " + response.data[i].rating.toUpperCase()); // create the <p> with the rating info
+                newDiv.append(newRating); // add the rating into the newDiv
+                $("#images").append(newDiv); // add the newDiv to the images div on the screen
+                scrubbedCount++; // increment scrubbedCount
+            } else {
+                // do nothing
+                console.log("doing nothing");
+            };
+            
         } // close for loop
     }); // close api call
 }); // close click watch
@@ -74,7 +93,10 @@ $(document).on("click", ".gifClick", function() { // watch for the click from on
 $("form").submit(function(event){ // watch for the form submit
     event.preventDefault(); // prevent the form from reloading the page
     console.log("gif addition click captured");
-    topics.push(document.getElementById('newGif').value) // push the newGif value from the text field into the gif topics array
+    // topics.push(newGif.val());
+    topics.push($("#newGif").val().trim());
+    // ^^ my attempt at cleaning up the line below but failing
+    // topics.push(document.getElementById('newGif').value) // push the newGif value from the text field into the gif topics array
     writeButtons(); // call write buttons to update the topics on the screen
 });
 
